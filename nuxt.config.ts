7eb869c2b926a3f8c6aa3c6fd1eb3e287a12a1da
Paweł Mierzwa <cxnshx@gmail.com>
2025-01-03 00:25:36 +0100

    Update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,32 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui', '@pinia/nuxt'],
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
+  runtimeConfig: {
+    jwtSecret: '',
+    jwtAlg: '',
+  },
+  nitro: {
+    experimental: {
+      database: true,
+    },
+    database: {
+      users: {
+        connector: 'sqlite',
+        options: {
+          name: 'users',
+        }
+      },
+      messages: {
+        connector: 'sqlite',
+        options: {
+          name: 'messages',
+        }
+      },
+    }
+  },
+
 })
