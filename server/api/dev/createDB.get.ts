3a8aa5ce73c517d39c1175cbc26d206efe291858
Paweł Mierzwa <cxnshx@gmail.com>
2025-01-04 03:00:10 +0100

    Fix dev routes

--- server/api/dev/createDB.get.ts
+++ server/api/dev/createDB.get.ts
@@ -10,17 +10,17 @@ export default defineEventHandler(async (event) => {
     if (!table) {
         return { code: 400, message: 'Table not specified', status: 'error' };
     }
-    if (table === 'users') {
-        await db.sql`DROP TABLE IF EXISTS users`;
-        await db.sql`CREATE TABLE IF NOT EXISTS users ("uid" TEXT PRIMARY KEY, "username" TEXT NOT NULL, "password" TEXT NOT NULL, "email" TEXT NOT NULL, "rooms" TEXT, "role" TEXT DEFAULT "0", "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`;
-        return new Response('Table "users" created');
+    if (table === 'accounts') {
+        await db.sql`DROP TABLE IF EXISTS accounts`;
+        await db.sql`CREATE TABLE IF NOT EXISTS accounts ("id" TEXT PRIMARY KEY, "username" TEXT NOT NULL, "password" TEXT NOT NULL, "email" TEXT NOT NULL, "rooms" TEXT, "role" TEXT DEFAULT "0", "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`;
+        return new Response('Table "accounts" created');
     } else if (table === 'rooms') {
         await db.sql`DROP TABLE IF EXISTS rooms`;
         await db.sql`CREATE TABLE IF NOT EXISTS rooms ("id" TEXT PRIMARY KEY, "views" INTEGER DEFAULT 0, "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`;
         return new Response('Table "rooms" created');
     } else if (table === 'messages') {
         await db.sql`DROP TABLE IF EXISTS messages`;
-        await db.sql`CREATE TABLE IF NOT EXISTS messages ("id" TEXT PRIMARY KEY, "roomId" TEXT, "userId" TEXT, "message" TEXT, "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`;
+        await db.sql`CREATE TABLE IF NOT EXISTS messages ("id" TEXT PRIMARY KEY, "roomId" TEXT NOT NULL, "userId" TEXT NOT NULL, "username" TEXT NOT NULL, "message" TEXT NOT NULL, "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`;
         return new Response('Table "messages" created');
     }
 });
--- server/api/dev/previewDB.get.ts
+++ server/api/dev/previewDB.get.ts
@@ -1,15 +1,18 @@
 export default defineEventHandler(async (event) => {
     const query = getQuery(event);
     const table = query.table;
-    const database = query.db;
+    let database = query.db;
+    if (!database) {
+        database = "users";
+    }
     const db = useDatabase(database as string);
     setResponseStatus(event, 200, "Success")
-    if (table === 'matches') {
-        return db.sql`SELECT * FROM matches`;
-    } else if (table === 'users') {
-        return db.sql`SELECT * FROM users`;
-    } else if (table === 'queue') {
-        return db.sql`SELECT * FROM queue`;
+    if (table === 'rooms') {
+        return db.sql`SELECT * FROM rooms`;
+    } else if (table === 'accounts') {
+        return db.sql`SELECT * FROM accounts`;
+    } else if (table === 'messages') {
+        return db.sql`SELECT * FROM messages`;
     }
     // list all tables of the default database 
     return db.sql`SELECT name FROM sqlite_master WHERE type='table'`;
