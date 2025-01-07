05b692d4f367e49b5debe9bcd74514a89bb61276
Paweł Mierzwa <cxnshx@gmail.com>
2025-01-07 01:24:08 +0100

    Add user profile page

---
+++ pages/user/[id].vue
@@ -0,0 +1,47 @@
+<template>
+    <UContainer class="flex flex-col items-center justify-between pt-32 md:p-32">
+        <h1 class="text-5xl">{{ data.username ? data.username + "'s" : 'Users' }} profile</h1>
+        <div class="flex flex-col items-center justify-between mt-10 w-full">
+            <div class="flex items-center gap-4">
+                <UAvatar :src="'https://i.pravatar.cc/64?u=' + id" size="lg" />
+                <div>
+                    <h2 class="text-2xl">{{ data.username }}</h2>
+                </div>
+            </div>
+            <UDivider class="my-4 w-1/2" />
+            <div v-if="data.stats" class="flex flex-col items-center justify-between p-4 bg-gray-100 dark:bg-gray-900 rounded-md">
+                <h3 class="text-xl">Stats</h3>
+                <table class="[&>*>tr>td]:p-2 w-full h-full">
+                    <tbody>
+                        <tr>
+                            <td class="font-bold">Messages sent:</td>
+                            <td>{{ data.stats.totalMessages }}</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold">Rooms created:</td>
+                            <td>{{ data.stats.totalRoomsDiscovered }}</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold">Rooms joined:</td>
+                            <td>{{ data.stats.totalRoomsJoined }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div v-else-if="data.message" class="text-center">
+                <p>Error fetching user data</p>
+                <p class="text-red-300">{{ data.code }}, {{ data.message }}</p>
+            </div>
+        </div>
+    </UContainer>
+</template>
+
+<script setup>
+definePageMeta({
+    middleware: 'auth',
+});
+
+const route = useRoute();
+const { id } = route.params;
+const { data } = await useFetch('/api/user/' + id + '/stats');
+</script>
