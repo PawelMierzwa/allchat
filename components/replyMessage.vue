5fc68585b3063972ebed6960e595d1aede82d045
PawelMierzwa <cxnshx@gmail.com>
2025-01-29 00:46:53 +0100

    Fix whitespace before text

--- components/replyMessage.vue
+++ components/replyMessage.vue
@@ -2,8 +2,7 @@
     <div :class="user.id === msg.sender.id ? 'mr-10' : 'ml-10'" class="whitespace-pre-wrap hybrid-break cursor-pointer"
         title="Go to original message" @click="$emit('goto', replyMsg.originalMsg.id)">
         <p :class="user.id === msg.sender.id ? 'text-end' : 'text-start'"
-            class="text-xs text-gray-500 dark:text-gray-400 group-hover:text-gray-600 dark:group-hover:text-gray-300">
-            replying to {{ replyMsg.originalMsg.sender.name }}: {{
+            class="text-xs text-gray-500 dark:text-gray-400 group-hover:text-gray-600 dark:group-hover:text-gray-300">replying to {{ replyMsg.originalMsg.sender.name }}: {{
                 removeReplyTag(replyMsg.originalMsg.content) }}
         </p>
         <p>{{ replyMsg.content }}</p>
