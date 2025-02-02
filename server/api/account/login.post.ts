abe93cfd68e45292c286ad815ffc4ff46f38f845
PawelMierzwa <cxnshx@gmail.com>
2025-02-02 20:43:32 +0100

    Add "remember me" feature

--- server/api/account/login.post.ts
+++ server/api/account/login.post.ts
@@ -2,42 +2,41 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt';
 
 export default defineEventHandler(async (event) => {
-    // Get the email and password from the request body
-    const { username, password } = await readBody(event);
+    const { username, password, remember } = await readBody(event);
 
-    // Validate the email and password
     if (!username || !password) {
         return { status: 400, message: 'Username and password are required' };
     }
 
-    // Get the database
+    if (username.length < 3 || password.length < 3) {
+        return { status: 400, message: 'Invalid username or password' };
+    }
+
     const db = useDatabase("users");
 
-    // Query for the user with the given email
     const result = await db.sql`SELECT * FROM accounts WHERE username = ${username}`;
     const rows = result?.rows ?? [];
 
-    // If the user does not exist, throw an error
     if (rows.length === 0) {
         return { status: 400, message: 'Invalid username or password' };
     }
 
-    // Get the user
     const user = rows[0];
-
-    // Compare the password
     const passwordMatch = await bcrypt.compare(password, user.password as string | '');
 
-    // If the password does not match, throw an error
     if (!passwordMatch) {
         return { status: 400, message: 'Invalid username or password' };
     }
 
-    // Generate a JWT token
     const secret = useRuntimeConfig(event).jwtSecret;
     const token = jwt.sign({ id: user.id, email: user.email, name: user.username }, secret as jwt.Secret);
-    setCookie(event, 'token', token);
-    // Return user data
+    
+    if (remember) {
+        setCookie(event, 'token', token, { expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30) });
+    } else {
+        setCookie(event, 'token', token);
+    }
+
     return {
         status: 200,
         message: {
